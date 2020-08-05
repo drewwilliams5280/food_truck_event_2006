2db30d23b12288866d51e4d93a19ea67a98b2ea4
Drew Williams <drewwilliams5280@gmail.com>
2020-08-05 09:22:21 -0600

    Adds files and sets up Event testing

---
+++ test/event_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+class EventTest < Minitest::Test
+
+
+end
