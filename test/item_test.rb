0f3372b80903c74b701d45a1cfb99a2c5f003098
Drew Williams <drewwilliams5280@gmail.com>
2020-08-05 09:00:05 -0600

    Sets up item test

---
+++ test/item_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/item'
+
+class ItemTest < Minitest::Test
+
+  def test_it_exists
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    assert_instance_of Item, item1
+    assert_instance_of Item, item2
+  end
+
+
+end
