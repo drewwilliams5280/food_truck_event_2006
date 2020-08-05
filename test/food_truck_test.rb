acdaf120cd59206b14469a5b671a11a659cb3ea3
Drew Williams <drewwilliams5280@gmail.com>
2020-08-05 09:07:38 -0600

    Sets up attribute test

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -6,9 +6,10 @@ require './lib/food_truck'
 
 class FoodTruckTest < Minitest::Test
 
-  def test_it_exists
+  def test_it_exists_with_attributes
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     assert_instance_of FoodTruck, food_truck
+    assert_equal "Rocky Mountain Pies", food_truck.name
   end
 
 
