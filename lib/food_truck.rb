6cf4c7c804b038d5868c4200592ac660918116a2
Drew Williams <drewwilliams5280@gmail.com>
2020-08-05 09:12:47 -0600

    Passes check stock test

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,17 @@
 class FoodTruck
 
-  attr_reader :name
+  attr_reader :name, :inventory
   def initialize(name)
     @name = name
+    @inventory = {}
+  end
+
+  def check_stock(item)
+    if @inventory.keys.include?(item)
+      @inventory[item]
+    else
+      0
+    end
   end
 
 end
