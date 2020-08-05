2e4f507bd3b80b9f17fad7b5686beb2e96c3bb36
Drew Williams <drewwilliams5280@gmail.com>
2020-08-05 09:02:16 -0600

    Sets up attribute test

--- test/item_test.rb
+++ test/item_test.rb
@@ -5,11 +5,13 @@ require './lib/item'
 
 class ItemTest < Minitest::Test
 
-  def test_it_exists
+  def test_it_exists_with_attributes
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     assert_instance_of Item, item1
     assert_instance_of Item, item2
+    assert_equal "Apple Pie (Slice)", item2.name
+    assert_equal 2.50, item2.price
   end
 
 
