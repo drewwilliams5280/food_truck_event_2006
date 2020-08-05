41246cd3e6a36444850bf487cf27b8df87193f3e
Drew Williams <drewwilliams5280@gmail.com>
2020-08-05 09:06:15 -0600

    Adds FoodTruck test setup

---
+++ test/food_truck_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/item'
+require './lib/food_truck'
+
+class FoodTruckTest < Minitest::Test
+
+  def test_it_exists
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_instance_of FoodTruck, food_truck
+  end
+
+
+end
